36d982c58a417c38d704b5c79449f5932984c81d
Micaël Dupuis <micaeldupuis@live.fr>
2022-06-20 16:57:36 +0200

    Ajout page au hasard

--- frontend/src/components/AuHasard.vue
+++ frontend/src/components/AuHasard.vue
@@ -1,7 +1,36 @@
-<script setup></script>
+<script setup>
+import { ref, computed } from "vue";
+import { useUserStore } from "@/services/userStore";
+import { useAPI } from "@/services/api";
+
+const { user } = useUserStore();
+const { getChaines } = useAPI();
+
+const chaines = ref([]);
+
+async function getData() {
+  chaines.value = await getChaines();
+}
+
+const rand = computed(() => {
+  if (chaines.value.length == 0) {
+    return "Rendez-vous sur la page CRUD pour ajouter une chaine";
+  } else if (user) {
+    let c = chaines.value.filter((chaine) => chaine.user === user.id);
+    return c.value[Math.floor(Math.random() * c.value.length)];
+  } else if (!user) {
+    return chaines.value[Math.floor(Math.random() * chaines.value.length)];
+  } else {
+    return "";
+  }
+});
+
+getData();
+</script>
 
 <template>
   <div class="row text-center">
     <h1>Au hasard</h1>
+    <p>{{ rand }}</p>
   </div>
 </template>
